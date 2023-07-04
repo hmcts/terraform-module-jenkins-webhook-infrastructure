e9ac42572e04325cca300cc6fe3e7a0dcd9ab227
mokainos <mohana.yalamanchi@justice.gov.uk>
2023-07-04 09:34:13 +0100

    update

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,6 @@
 output "logicapp-trigger-endpoint" {
   value = jsondecode(azurerm_resource_group_template_deployment.logic_app_deployment.output_content).callbackUrl.value
 }
+output "apiconnection_id" {
+  value = data.azurerm_managed_api.api.id
+}
