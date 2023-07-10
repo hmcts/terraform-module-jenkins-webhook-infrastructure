07388a6524fa9942cbbd530d47f58ab1fc93ca43
mokainos <72137340+mokainos@users.noreply.github.com>
2023-07-10 07:55:04 +0100

    Update data.tf

--- data.tf
+++ data.tf
@@ -12,6 +12,6 @@ locals {
   rg_name      = strcontains(var.product, "sds") ? var.env : local.mgmt_env
 }
 data "azurerm_user_assigned_identity" "keda" {
-  name                = "keda-${local.gmt_env}-mi"
+  name                = "keda-${local.mgmt_env}-mi"
   resource_group_name = "managed-identities-${local.rg_name}-rg"
 }
